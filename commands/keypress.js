b8f22f57ed436dbe808942f4aaf2a7476f9614bf
SABRINA TEE <sabrinatee@me.com>
2016-01-29 13:01:24 -0800

    Keypress cmds

--- commands/keypress.js
+++ commands/keypress.js
@@ -1,6 +1,6 @@
 const bb8 = require(__dirname + '/../lib/uuid-config')();
 const config = require(__dirname + '/../lib/uuid-config').uuid;
-const keypress = require("keypress");
+const keypress = require('keypress');
 
 module.exports = exports = () => {
   bb8.connect(listen);
@@ -9,42 +9,47 @@ module.exports = exports = () => {
     var stop = bb8.roll.bind(bb8, 0, 0);
     var roll = bb8.roll.bind(bb8, 200);
 
-    if (key.ctrl && key.name === "c") {
+    if (key.ctrl && key.name === 'c') {
       process.stdin.pause();
       process.exit();
     }
 
-    if (key.name === "up") {
+    if (key.name === 'up') {
       roll(0);
+      console.log('up');
       bb8.randomColor();
     }
 
-    if (key.name === "down") {
+    if (key.name === 'down') {
       roll(180);
+      console.log('down');
       bb8.randomColor();
     }
 
-    if (key.name === "left") {
+    if (key.name === 'left') {
       roll(270);
+      console.log('left');
       bb8.randomColor();
     }
 
-    if (key.name === "right") {
+    if (key.name === 'right') {
       roll(90);
+      console.log('right');
       bb8.randomColor();
     }
 
-    if (key.name === "space") {
+    if (key.name === 'space') {
       stop();
+      console.log('stop');
       bb8.color('red');
     }
   }
 
   function listen() {
     keypress(process.stdin);
-    process.stdin.on("keypress", handle);
+    process.stdin.on('keypress', handle);
 
-    console.log("Listening for arrow key presses");
+    console.log('control with keypress');
 
     process.stdin.setRawMode(true);
     process.stdin.resume();
